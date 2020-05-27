bb0eb9d6ac6b3cb673ff76a113af363608a14b01
pieru <aran0204.ambro@icloud.com>
2020-05-27 13:50:25 +0900

    class based view

--- helloworldproject/helloworldproject/urls.py
+++ helloworldproject/helloworldproject/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import helloworldfunction
+from .views import helloworldfunction, HelloWorldView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('helloworld', helloworldfunction),
+    path('helloworld/', helloworldfunction),
+    path('helloworldb/', HelloWorldView.as_view()),
 ]
